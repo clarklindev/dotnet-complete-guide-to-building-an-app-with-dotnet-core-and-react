601ebcf6f413eec29485b49c0943cae19e535f13
clark <clarklindev@gmail.com>
2025-11-13 19:22:49 +0200

    Building a walking skeleton Part 1 - .Net API - Creating the .Net projects

--- reactivities/API/obj/Debug/net10.0/API.AssemblyInfo.cs
+++ reactivities/API/obj/Debug/net10.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2065eef9602cdc4096aa8786bfdf46154a60505b")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
